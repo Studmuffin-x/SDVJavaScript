59358674322bf711433018c042f855be1bd94ceb
Jedidiah Thorp <jed-thorp@live.nmit.ac.nz>
2021-03-15 12:49:25 +1300

    week3 let test

---
+++ Src/week4.js
@@ -0,0 +1,20 @@
+// review   
+/** 
+ Values & Variables
+ */
+
+ // let = decaling variable 
+
+ let Country = "New Zealand"
+ let City = "Nelson"
+ let PostalCode = "7010"
+
+console.log(Country) ;
+console.log("nelson") ;
+console.log("7010") ;
+
+let isisland = true
+let language 
+
+console.log (typeof isisland) ;
+console.log (language)
